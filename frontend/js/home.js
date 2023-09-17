47610ba90143fa008f8b8e798d29d9a985faece6
jules-jean-louis1 <jules.jean-louis@laplateforme.io>
2023-09-17 17:56:09 +0200

    add admin page for add product

--- frontend/js/home.js
+++ frontend/js/home.js
@@ -6,5 +6,12 @@ const btnLogin = document.getElementById('btnLogin');
 if (btnLogin) {
     loginRegisterForm(btnLogin);
 }
-console.log(localStorage.getItem('user'));
-console.log(localStorage.getItem('token'));
+//console.log(localStorage.getItem('user'));
+const userToken = localStorage.getItem('token');
+
+if (userToken) {
+    const btnMenu = document.getElementById('btnMenu');
+    btnMenu.classList.remove('hidden');
+} else {
+    btnMenu.innerHTML = '';
+}
